a6f1f995c08392961c2017b5bf922a8fc654fe1d
Christian Ohlsson <crille@crille.org>
2017-12-28 14:50:17 +0100

    Kommentarsfixar

--- 3.py
+++ 3.py
@@ -1,11 +1,11 @@
 # practicepython.org
 # Övning 3
+#
 # Take a list, say for example this one:
 # myList = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # and write a program that prints out all the elements of the list that are less than 5.
 
 # Extras:
-
 # 1. Instead of printing the elements one by one, make a new list that has all the 
 # elements less than 5 from this list in it and print out this new list.
 # Write this in one line of Python.
