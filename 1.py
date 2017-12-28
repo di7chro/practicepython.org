0f2dbe0a16f671e12a18eef982f134b42c8010d6
Christian Ohlsson <crille@crille.org>
2017-12-28 10:51:43 +0100

    Extrauppgifter till Övning 1

--- 1.py
+++ 1.py
@@ -1,6 +1,18 @@
-name = input("Vad heter du? ")
-number = input("Ange ett tal: ")
+# practicepython.org
+# Övning 1
+# Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
-number = int(number)
-for i in range(number):
+import datetime
+def main():
+    name = input("Vad heter du? ")
+    age  = input("Ange din ålder: ")
+
+    age = int(age)                  # Konvertera ålder till heltal
+    now = datetime.datetime.now()   # Hämta in tiden just nu
+    this_year = now.year            # Spara undan aktuellt år
+    turn100 = this_year - age + 100 # Räkna ut årtalet man fyller 100
+    
     print("Hejsan ", name)
+    print("Du fyller 100 år", turn100)
+if __name__ == "__main__": main()
---
+++ 2.py
@@ -0,0 +1,21 @@
+# practicepython.org
+# Övning 2
+#
+# Ask the user for a number. 
+# Depending on whether the number is even or odd, print out an appropriate 
+# message to the user. 
+# Extras:
+# If the number is a multiple of 4, print out a different message.
+
+def main():
+    siffra = input("Ange en siffra: ")
+    siffra = int(siffra)
+    if siffra % 4 == 0:
+        print("Du skrev in {} vilket är JÄMNT tal som är delbart med 4.".format(siffra))        
+    elif siffra % 2 == 0:
+        print("Du skrev in {} vilket är JÄMNT tal".format(siffra))
+    else:
+        print("Siffran {} är ett UDDA tal".format(siffra))
+        
+if __name__ == "__main__": main()
+
