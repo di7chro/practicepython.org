1f09da7c80b42088d300a6b0ff04e02853107dd5
Christian Ohlsson <crille@crille.org>
2017-12-29 18:52:26 +0100

    Rättstavning

--- gissa.py
+++ gissa.py
@@ -5,7 +5,7 @@
 from random import randint
 
 namn = input("Välkommen! Vad heter du? ")
-print("Hej {}! Nu ska du gissa på ett tal melölan 0 och 10.".format(namn))
+print("Hej {}! Nu ska du gissa på ett tal mellan 0 och 10.".format(namn))
 
 hemligt = randint(0,10)
 antal = 0
