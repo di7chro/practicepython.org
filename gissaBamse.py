a1a08c18bdfc8b08fa0fcf78c67b1901c0493998
Christian Ohlsson <crille@crille.org>
2018-02-19 10:43:36 +0100

    Gissa Bamse-karaktärer

---
+++ gissaBamse.py
@@ -0,0 +1,21 @@
+# Gissningsspel av Emma Ohlsson
+# Gissa karaktärer från Bamse
+# 19 Februari 2018
+#
+
+from random import randint
+
+Lista=["bamse","skalman","farmor","brummelisa","lilleskutt","vargen","brumme"]
+
+namn=input("Välkommen! Vad heter du? ")
+
+print("Hej {}! Gissa på någon i Bamse.".format (namn))
+
+hemlig=randint(0,len(Lista))
+
+while True:
+    Gissning=input("Vem gissar du på? ")
+    if Gissning==Lista[hemlig]:
+        break
+
+print("Bra jobbat {}! Du gissade rätt!".format (namn))
