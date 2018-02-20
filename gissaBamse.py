88d5cf56b9771ab43a335eb94574c4c5991baac4
Christian Ohlsson <crille@crille.org>
2018-02-20 18:56:09 +0100

    Antal gissningar skrivs ut

--- gissaBamse.py
+++ gissaBamse.py
@@ -4,18 +4,16 @@
 #
 
 from random import randint
-
 Lista=["bamse","skalman","farmor","brummelisa","lilleskutt","vargen","brumme"]
-
 namn=input("Välkomm en! Vad heter du? ")
-
 print("Hej {}! Gissa på någon i Bamse.".format (namn))
-
 hemlig=randint(0,len(Lista))
+antal = 0
 
 while True:
+    antal = antal + 1
     Gissning=input("Vem gissar du på? ")
     if Gissning==Lista[hemlig]:
         break
 
-print("Bra jobbat {}! Du gissade rätt!".format (namn))
+print("Bra jobbat {}! Du gissade rätt efter {} försök!".format (namn, antal))
